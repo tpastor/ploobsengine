969446ea690f785e6fb6492bc59b80cc2b1745e7
ThiagoPastor <ThiagoPastor@localhost>
2011-06-04 19:14:59 -0300

    texture scale 2D ghost added

---
+++ PloobsEngine/PloobsEngine/Physics2D/Farseer/GhostObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PloobsEngine.Physics2D.Farseer
+{
+    public class GhostObject : I2DPhysicObject
+    {
+        public GhostObject(Vector2 Position, float rotation = 0)
+        {
+            this.Enabled = false;
+            this.Position = Position;
+            this.Rotation = rotation;
+            this.Origin = Vector2.Zero;
+        }
+
+        public override bool isDynamic
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {                
+            }
+        }
+    }
+}
