77c70c892f936b03c5821bcfd1ef3af9aa555d99
ThiagoPastor <ThiagoPastor@localhost>
2011-06-13 00:15:19 -0300

    folders refactoring

--- PloobsEngine/PloobsEngine/Modelo2D/SpriteFarseer.cs
+++ PloobsEngine/PloobsEngine/Modelo2D/SpriteFarseer.cs
@@ -8,17 +8,18 @@ using FarseerPhysics.Common;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using FarseerPhysics.Collision.Shapes;
+using PloobsEngine.Physics2D.Farseer;
 
 namespace PloobsEngine.Modelo2D
 {
     public class SpriteFarseer : IModelo2D
     {
-        static Asset2DCreator assetCreator = null;
+        static FarseerAsset2DCreator assetCreator = null;
 
         public SpriteFarseer(GraphicFactory factory, Vertices vertices, Color color, float materialScale = 1) :base(ModelType.Texture)
         {
             if(assetCreator ==null)
-                assetCreator  = new Asset2DCreator(factory);
+                assetCreator  = new FarseerAsset2DCreator(factory);
 
             Texture = assetCreator.CreateTextureFromVertices(vertices,color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
@@ -28,7 +29,7 @@ namespace PloobsEngine.Modelo2D
             : base(ModelType.Texture)
         {
             if (assetCreator == null)
-                assetCreator = new Asset2DCreator(factory);
+                assetCreator = new FarseerAsset2DCreator(factory);
 
             Texture = assetCreator.CreateTextureFromVertices(shape.Vertices, color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
@@ -38,7 +39,7 @@ namespace PloobsEngine.Modelo2D
             : base(ModelType.Texture)
         {
             if (assetCreator == null)
-                assetCreator = new Asset2DCreator(factory);
+                assetCreator = new FarseerAsset2DCreator(factory);
 
             Texture = assetCreator.CreateCircleTexture(shape.Radius, color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
@@ -48,7 +49,7 @@ namespace PloobsEngine.Modelo2D
             : base(ModelType.Texture)
         {
             if (assetCreator == null)
-                assetCreator = new Asset2DCreator(factory);
+                assetCreator = new FarseerAsset2DCreator(factory);
 
             Texture = assetCreator.CreateCircleTexture(radius, color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
