fa6fc2165eaf9b0a27086b47c8f18303556300ca
ThiagoPastor <ThiagoPastor@localhost>
2011-09-18 11:19:29 -0300

    integrate the heat and nigh vision shader with the engine

--- PloobsEngine/PloobsEngine/SceneControl/Scene/PostEffects/HeatPostEffect.cs
+++ PloobsEngine/PloobsEngine/SceneControl/Scene/PostEffects/HeatPostEffect.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿#region License
+/*
+    PloobsEngine Game Engine Version 0.3 Beta
+    Copyright (C) 2011  Ploobs
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+#if !WINDOWS_PHONE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PloobsEngine.SceneControl;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace EngineTestes.Post
+namespace PloobsEngine.SceneControl
 {
-    class HeatPostEffect : IPostEffect
+    public class HeatPostEffect : IPostEffect
     {
         public HeatPostEffect()
             : base(PostEffectType.All)
@@ -16,7 +36,7 @@ namespace EngineTestes.Post
 
         public override void Init(PloobsEngine.Engine.GraphicInfo ginfo, PloobsEngine.Engine.GraphicFactory factory)
         {
-            effect = factory.GetEffect("Effects/heat");
+            effect = factory.GetEffect("Heat",false,true);
         }
         Effect effect;
         public override void Draw(Microsoft.Xna.Framework.Graphics.Texture2D ImageToProcess, RenderHelper rHelper, Microsoft.Xna.Framework.GameTime gt, PloobsEngine.Engine.GraphicInfo GraphicInfo, IWorld world, bool useFloatBuffer)
@@ -28,3 +48,4 @@ namespace EngineTestes.Post
         }
     }
 }
+#endif
