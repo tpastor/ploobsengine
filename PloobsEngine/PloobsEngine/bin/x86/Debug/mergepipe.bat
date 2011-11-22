dc783264ad88f82830dee1f7f8f2595208a5e4f5
thiago dias pastor <thiago.dias.pastor@MW7RH1WRH1KIRI.dir.svc.accenture.com>
2011-11-22 11:23:55 -0200

    obj importer added

--- PloobsEngine/PloobsEngine/bin/x86/Debug/mergepipe.bat
+++ PloobsEngine/PloobsEngine/bin/x86/Debug/mergepipe.bat
@@ -1,3 +1,3 @@
-ilmerge /out:PloobsEnginePipelineDebug.dll PipelineExtension.dll Library.dll TextureGammaDecodeAndLinearMipMap.dll TomShane.Neoforce.Importers.dll XNAnimationPipeline.dll XNAnimation.dll   /lib:"C:\Program Files (x86)\Microsoft XNA\XNA Game Studio\v4.0\References\Windows\x86" /targetplatform:v4,"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /keyfile:XNAnimation.snk  /ver:0.0.0.1 /log 
+ilmerge /out:PloobsEnginePipelineDebug.dll ObjImporter.dll PipelineExtension.dll Library.dll TextureGammaDecodeAndLinearMipMap.dll TomShane.Neoforce.Importers.dll XNAnimationPipeline.dll XNAnimation.dll   /lib:"C:\Program Files (x86)\Microsoft XNA\XNA Game Studio\v4.0\References\Windows\x86" /targetplatform:v4,"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /keyfile:XNAnimation.snk  /ver:0.0.0.1 /log 
 pause
 
