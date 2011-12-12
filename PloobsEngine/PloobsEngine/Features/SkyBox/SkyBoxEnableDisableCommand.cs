4b0fcda945d46a51f8f99c52280b38cdc04bcf6f
thiago dias pastor <thiago.dias.pastor@MW7RH1WRH1KIRI.dir.svc.accenture.com>
2011-12-12 17:16:29 -0200

    phone skybox not sharing content across phone versions anymore

--- PloobsEngine/PloobsEngine/Features/SkyBox/SkyBoxEnableDisableCommand.cs
+++ PloobsEngine/PloobsEngine/Features/SkyBox/SkyBoxEnableDisableCommand.cs
@@ -17,7 +17,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #endregion
-#if !WINDOWS_PHONE
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -66,4 +66,3 @@ namespace PloobsEngine.Features
     }
     
 }
-#endif
