921e79a0b39d17729bf455972738c1815bf0a007
thiago dias pastor <thiago.dias.pastor@MW7RH1WRH1KIRI.dir.svc.accenture.com>
2011-12-13 19:13:06 -0200

    2D unified coordinate system fixed

--- PloobsEngine/PloobsEngine/Modelo2D/SpriteFarseer.cs
+++ PloobsEngine/PloobsEngine/Modelo2D/SpriteFarseer.cs
@@ -35,40 +35,78 @@ namespace PloobsEngine.Modelo2D
     {
         static FarseerAsset2DCreator assetCreator = null;
 
-        public SpriteFarseer(GraphicFactory factory, Vertices vertices, Color color, float materialScale = 1) :base(ModelType.Texture)
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="vertices"></param>
+        /// <param name="color"></param>
+        /// <param name="isInSimulationUnits"></param>
+        /// <param name="materialScale"></param>
+        public SpriteFarseer(GraphicFactory factory, Vertices vertices, Color color, bool isInSimulationUnits = false,float materialScale = 1)
+            : base(ModelType.Texture)
         {
             if(assetCreator ==null)
                 assetCreator  = new FarseerAsset2DCreator(factory);
+
+            if (isInSimulationUnits)
+            {
+                for (int i = 0; i < vertices.Count; i++)
+                {
+                    vertices[i] = ConvertUnits.ToSimUnits(vertices[i]);
+                }
+            }
 
             Texture = assetCreator.CreateTextureFromVertices(vertices,color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
         }
 
-        public SpriteFarseer(GraphicFactory factory, PolygonShape shape, Color color, float materialScale = 1)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="shape"></param>
+        /// <param name="color"></param>
+        /// <param name="isInSimulationUnits"></param>
+        /// <param name="materialScale"></param>
+        public SpriteFarseer(GraphicFactory factory, PolygonShape shape, Color color, bool isInSimulationUnits = false, float materialScale = 1)
             : base(ModelType.Texture)
         {
             if (assetCreator == null)
                 assetCreator = new FarseerAsset2DCreator(factory);
+
+            if (isInSimulationUnits)
+            {
+                for (int i = 0; i < shape.Vertices.Count; i++)
+                {
+                    shape.Vertices[i] = ConvertUnits.ToSimUnits(shape.Vertices[i]);
+                }
+            }
 
             Texture = assetCreator.CreateTextureFromVertices(shape.Vertices, color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
         }
 
-        public SpriteFarseer(GraphicFactory factory, CircleShape shape, Color color, float materialScale = 1)
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="factory"></param>
+       /// <param name="radius"></param>
+       /// <param name="color"></param>
+       /// <param name="isInSimulationUnits"></param>
+       /// <param name="materialScale"></param>
+         public SpriteFarseer(GraphicFactory factory, float radius, Color color, bool isInSimulationUnits = false, float materialScale = 1)
             : base(ModelType.Texture)
         {
             if (assetCreator == null)
                 assetCreator = new FarseerAsset2DCreator(factory);
 
-            Texture = assetCreator.CreateCircleTexture(shape.Radius, color, materialScale);
-            Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
-        }
+            if (isInSimulationUnits)
+            {
+                radius = ConvertUnits.ToSimUnits(radius);
+            }
 
-        public SpriteFarseer(GraphicFactory factory, float radius, Color color, float materialScale = 1)
-            : base(ModelType.Texture)
-        {
-            if (assetCreator == null)
-                assetCreator = new FarseerAsset2DCreator(factory);
 
             Texture = assetCreator.CreateCircleTexture(radius, color, materialScale);
             Origin = new Vector2(Texture.Width / 2f, Texture.Height / 2f);
