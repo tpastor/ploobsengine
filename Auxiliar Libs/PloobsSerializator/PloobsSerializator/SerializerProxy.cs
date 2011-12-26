da3fc0f57f673c93d8c61eeefb77dd888c042957
ThiagoPastor <ThiagoPastor@localhost>
2011-12-26 21:32:02 -0200

    Added ploobsengine serializator project

---
+++ Auxiliar Libs/PloobsSerializator/PloobsSerializator/SerializerProxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace PloobsSerializator
+{
+    public class SerializerProxy
+    {
+        internal SerializatorMachine SerializatorMachine ;
+        internal XmlSerializer Serializer;
+
+        public void Serialize(String fieldname, object obj)
+        {
+            SerializatorMachine.SerializeType(fieldname, obj, Serializer);
+        }
+    }
+
+    public class DeSerializerProxy
+    {
+        internal XElement current;
+        internal SerializatorMachine SerializatorMachine;
+        internal XMLDeserializer XMLDeserializer;
+
+        public Object DeSerialize(String fieldname)
+        {
+            var matches = from el in current.Elements()
+                          where el.Name == fieldname
+                          select el;
+            var mat = matches.First();
+
+            return XMLDeserializer.populateobj(mat);            
+        }
+    }
+}
