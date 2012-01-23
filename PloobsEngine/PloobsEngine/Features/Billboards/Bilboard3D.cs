cad57a6d16aaa1f7eac32427c7e2adc5de2920cd
thiago dias pastor <thiago.dias.pastor@MW7RH1WRH1KIRI.dir.svc.accenture.com>
2012-01-23 16:13:22 -0200

    [PloobsEngine] CPUBillboards now can follow IObjects

--- PloobsEngine/PloobsEngine/Features/Billboards/Bilboard3D.cs
+++ PloobsEngine/PloobsEngine/Features/Billboards/Bilboard3D.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using PloobsEngine.SceneControl;
 
 namespace PloobsEngine.Features.Billboard
 {
@@ -15,9 +16,30 @@ namespace PloobsEngine.Features.Billboard
             this.Position = pos;
             this.Scale = scale;
         }
+
+        public Billboard3D(Texture2D tex,IObject obj, Vector3 displacement, Vector2 scale)
+        {
+            this.Texture = tex;
+            this.Position = displacement;
+            this.obj = obj;
+            this.Scale = scale;
+        }
         public Vector2 Scale;
         public Texture2D Texture;
-        public Vector3 Position;
+        private Vector3 position;
+        IObject obj;
+        public Vector3 Position
+        {
+            get
+            {
+                return obj == null ? position : obj.PhysicObject.Position + position;
+            }
+            set
+            {
+                this.position = value;
+                obj = null;
+            }
+        }
 
     }
 
@@ -32,11 +54,34 @@ namespace PloobsEngine.Features.Billboard
             this.Scale = scale;
             this.SpriteFont = SpriteFont;
         }
+
+        public TextBillboard3D(String tex, Color color, IObject obj, Vector3 displacement, float scale, SpriteFont SpriteFont = null)
+        {
+            this.Color = color;
+            this.Message = tex;
+            this.obj = obj;
+            this.position = displacement;
+            this.Scale = scale;
+            this.SpriteFont = SpriteFont;
+        }
         public SpriteFont SpriteFont;
         public Color Color;
         public float Scale;
         public String Message;
-        public Vector3 Position;
+        IObject obj;
 
+        private Vector3 position;
+        public Vector3 Position
+        {
+            get
+            {
+                return obj == null ? position : obj.PhysicObject.Position + position;
+            }
+            set
+            {
+                this.position = value;
+                obj = null;
+            }
+        }
     }
 }
