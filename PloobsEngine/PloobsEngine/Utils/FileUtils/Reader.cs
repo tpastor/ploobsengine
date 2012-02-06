27034efb4650579299c39b15fede13febf8b6474
thiago dias pastor <thiago.dias.pastor@MW7RH1WRH1KIRI.dir.svc.accenture.com>
2012-02-06 15:44:12 -0200

    [PloobsEngine] File Readers and Writers Added [Engine Tests] Added a Simple Demo to tests File readers/Writers

---
+++ PloobsEngine/PloobsEngine/Utils/FileUtils/Reader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+///Credits to Mathias Gehlhaar
+namespace PloobsEngine.Utils
+{
+    public abstract class Reader
+    {
+        /// <summary>
+        /// Liest das gesamte Dokument
+        /// </summary>
+        public abstract void Read(char separator = '=', char endOfLine = ';', char comment = '%');
+
+        /// <summary>
+        /// Liest den Wert eines Keys aus dem Dokument heraus.
+        /// </summary>
+        /// <param name="key">der Schlüssel: "resolution = 'Wert'"</param>
+        public abstract String ReadValue(string key);
+
+    }
+}
