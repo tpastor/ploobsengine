26009099610245cdf6a344b7d2214b24d159b425
ThiagoPastor <ThiagoPastor@localhost>
2012-02-12 18:13:38 -0200

    [PloobsEngine] Merge Scripts Updated

--- PloobsEngine/PloobsEngine/bin/x86/Release/mergepipe.bat
+++ PloobsEngine/PloobsEngine/bin/x86/Release/mergepipe.bat
@@ -1,3 +1,3 @@
 ilmerge /out:PloobsEnginePipelineDebug.dll PipelineExtension.dll ObjImporter.dll Library.dll TextureGammaDecodeAndLinearMipMap.dll TomShane.Neoforce.Controls.dll TomShane.Neoforce.Importers.dll XNAnimationPipeline.dll XNAnimation.dll   /lib:"C:\Program Files (x86)\Microsoft XNA\XNA Game Studio\v4.0\References\Windows\x86" /targetplatform:v4,"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /keyfile:XNAnimation.snk  /ver:0.0.0.1 /log 
-pause
+
 
