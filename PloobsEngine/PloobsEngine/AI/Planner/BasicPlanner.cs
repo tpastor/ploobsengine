421948aed1024c67bb5fafde91f9c7d759b53032
ThiagoPastor <ThiagoPastor@localhost>
2012-03-19 23:38:24 -0300

    [PloobsEngine] AI Initial Goal Planner Added

---
+++ PloobsEngine/PloobsEngine/AI/Planner/BasicPlanner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PloobsEngine.IA
+{
+    public class BasicPlanner : IPlanner
+    {
+
+        public override PlanSet CreatePlan(WorldState actual, Goal destiny)
+        {
+            PlanSet PlanSet = new PlanSet();
+                
+            WorldState goal = destiny.WorldState.Clone();
+
+            WorldState current = actual.Clone();            
+
+            while (!destiny.WorldState.isCompatibleSource(current))
+            {
+                Action act = null;
+                foreach (var item in Actions)
+                {
+                    if (item.GetPreConditions().isCompatibleSource(current))
+                    {
+                        if (item.ProceduralPreConditions())
+                        {
+                            act = item;
+                            break;
+                        }
+                    }
+                }
+
+                if (act != null)
+                {
+                    foreach (var item in act.GetEffects().GetSymbols())
+                    {
+                        current.SetSymbol(item.Clone());
+                    }
+                    act.ApplyEffects();
+                }
+                else
+                {
+                    return null;
+                }
+
+                
+                System.Diagnostics.Debug.WriteLine(act.Name);
+            }
+            System.Diagnostics.Debug.WriteLine(destiny.Name);
+           return PlanSet;
+        }
+    }
+}
---
+++ PloobsEngine/PloobsEngine/AI/Planner/IPlanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PloobsEngine.IA
+{
+    public abstract class IPlanner
+    {
+        public List<Action> Actions = new List<Action>();
+        
+        public abstract PlanSet CreatePlan(WorldState actual, Goal destiny);
+    }
+}
