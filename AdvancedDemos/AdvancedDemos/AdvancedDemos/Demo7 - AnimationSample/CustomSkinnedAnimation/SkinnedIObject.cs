ad4fdbfeeca76835023e537538984edc77778a27
ThiagoPastor <ThiagoPastor@localhost>
2012-03-27 00:12:36 -0300

    [Advanced Demos] Improved Animation Demo

--- AdvancedDemos/AdvancedDemos/AdvancedDemos/Demo7 - AnimationSample/CustomSkinnedAnimation/SkinnedIObject.cs
+++ AdvancedDemos/AdvancedDemos/AdvancedDemos/Demo7 - AnimationSample/CustomSkinnedAnimation/SkinnedIObject.cs
@@ -36,13 +36,33 @@ namespace PloobsProjectTemplate
             private set;
         }
 
+        float headRotation = 0;
+        float step = 0.01f;
         protected override void UpdateObject(Microsoft.Xna.Framework.GameTime gt, PloobsEngine.Cameras.ICamera cam, IWorld world)
         {
-            SkinnedModel.AnimationPlayer.Update(gt.ElapsedGameTime, true, Matrix.Identity);
-            Microsoft.Xna.Framework.Matrix[] bones;
-            bones = SkinnedModel.AnimationPlayer.GetSkinTransforms();
-            ///You can manipulate the bones here !!! The way you want
-            ForwardSkinnedShader.SetBones(bones);
+            // Tell the animation player to compute the latest bone transform matrices.
+            SkinnedModel.AnimationPlayer.UpdateBoneTransforms(gt.ElapsedGameTime, true);
+
+            Matrix[] boneTransforms = new Matrix[SkinnedModel.SkinningData.BindPose.Count];
+            // Copy the transforms into our own array, so we can safely modify the values.
+            SkinnedModel.AnimationPlayer.GetBoneTransforms().CopyTo(boneTransforms, 0);
+
+            // Modify the transform matrices for the head and upper-left arm bones.
+            int headIndex = SkinnedModel.SkinningData.BoneIndices["Head"];
+
+            ///some bone manipulation            
+            headRotation += step;
+            if (headRotation > 0.7f || headRotation < -0.7f)
+                step *= -1;            
+
+            Matrix headTransform = Matrix.CreateRotationX(headRotation);            
+            boneTransforms[headIndex] = headTransform * boneTransforms[headIndex];
+
+            // Tell the animation player to recompute the world and skin matrices.
+            SkinnedModel.AnimationPlayer.UpdateWorldTransforms(Matrix.Identity, boneTransforms);
+            SkinnedModel.AnimationPlayer.UpdateSkinTransforms();
+
+            ForwardSkinnedShader.SetBones(SkinnedModel.AnimationPlayer.GetSkinTransforms());
             base.UpdateObject(gt, cam, world);
         }
     }
