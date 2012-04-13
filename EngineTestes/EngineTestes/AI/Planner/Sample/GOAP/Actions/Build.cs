9c86bfe004c821e37d2cf68711b3725ffbd92f81
thiago dias pastor <thiago.dias.pastor@MW7RH1WRH1KIRI.dir.svc.accenture.com>
2012-04-12 21:37:34 -0300

    [PloobsEngine] BlackBoard Fix and Tests

---
+++ EngineTestes/EngineTestes/AI/Planner/Sample/GOAP/Actions/Build.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PloobsEngine.IA;
+
+namespace Settlers.BlackBoardControl.GOAP.Actions
+{
+        public class Build : PloobsEngine.IA.Action
+        {
+            public Build(Dictionary<String,int> resourcesNeeded, String houseName, int cost)
+            {
+                this.Cost = cost;
+                Name = "Build";                
+                this.houseName = houseName;
+                this.resourcesNeeded = resourcesNeeded;
+            }
+            Dictionary<String, int> resourcesNeeded;
+            String houseName;
+
+            public override bool ProceduralPreConditions(WorldState WorldState)
+            {
+                foreach (var item in resourcesNeeded)
+                {
+                    if (WorldState.GetSymbolValue<int>(item.Key) < item.Value)
+                        return false;
+                }
+                return true;
+            }
+
+            public override void ApplyEffects(PloobsEngine.IA.WorldState WorldState)
+            {
+                ////remove the resources
+                foreach (var item in resourcesNeeded)
+                {
+                    WorldSymbol WorldSymbol2 = WorldState.GetSymbol(item.Key);
+                    WorldSymbol2.SetSymbol<int>(WorldSymbol2.GetSymbol<int>() - item.Value);
+                    WorldState.SetSymbol(WorldSymbol2);
+                }
+
+
+                ///add one house
+                WorldSymbol WorldSymbol = WorldState.GetSymbol(houseName);
+                if(WorldSymbol == null)
+                {
+                    WorldSymbol = new WorldSymbol(houseName, 1);
+                    WorldState.SetSymbol(WorldSymbol);
+                }
+                else
+                {
+                    int val = WorldSymbol.GetSymbol<int>();
+                    WorldSymbol.SetSymbol(val + 1);                    
+                    WorldState.SetSymbol(WorldSymbol);
+                }                
+
+                base.ApplyEffects(WorldState);
+            }
+
+        }
+    }
---
+++ EngineTestes/EngineTestes/AI/Planner/Sample/GOAP/Actions/Patrol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PloobsEngine.IA;
+
+namespace Settlers
+{
+    public class Patrol : PloobsEngine.IA.Action
+    {
+        public Patrol()
+        {
+            this.Cost = 10;
+            Name = "Patrol";                        
+        }
+
+    }
+}
